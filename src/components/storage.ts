9d7b02911a1bc3c9ea8f11e4b5cf7fecd07194be
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2024-01-06 19:39:30 +0100

    feat: add session/local storage

---
+++ src/components/storage.ts
@@ -0,0 +1,27 @@
+import {mount, tags, type Component} from '@tentjs/tent'
+
+const {ul, li} = tags
+
+type State = {
+  list: string[]
+}
+
+const initial = JSON.parse(sessionStorage.getItem('storage') ?? '[]')
+
+const Storage: Component<State> = {
+  state: {list: initial},
+  view: ({state}) => ul(state.list.map(
+    item => li(item)
+  )),
+  mounted: ({state}) => {
+    if (!state.list.length) {
+      state.list = ['Item #1', 'Item #2', 'Item #3']
+      sessionStorage.setItem('storage', JSON.stringify(state.list))
+    }
+  }
+}
+
+mount(
+  document.querySelector('.recipe'),
+  Storage
+)

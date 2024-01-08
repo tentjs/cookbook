8edbcee6b6b8d9a5abd7fa8fe97be82694ff17e9
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2024-01-08 21:56:10 +0100

    feat: add #daretoshare24 input

---
+++ src/components/input.ts
@@ -0,0 +1,26 @@
+import {mount, tags, type Component} from '@tentjs/tent'
+import * as styles from '../css/input.module.css'
+
+const {div, p, input} = tags
+
+type State = {
+  text: string
+}
+
+const Text: Component<State> = {
+  state: {text: ''},
+  view: ({state}) => div([
+    div([
+      input([], {
+        oninput: ({target}) => state.text = target.value,
+        placeholder: 'Your message...'
+      }),
+      p(state.text),
+    ], {className: styles.group})
+  ])
+}
+
+mount(
+  document.querySelector('.recipe'),
+  Text,
+)

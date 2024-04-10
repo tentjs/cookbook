d8ad24b70f82e00e0d42a90bfa60df37822a6c7c
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2024-04-10 17:31:33 +0200

    feat: label

---
+++ src/components/label.ts
@@ -0,0 +1,29 @@
+import {mount, tags} from "@tentjs/tent";
+import * as styles from "../css/label.module.css";
+
+const {span} = tags
+
+const Label = {
+  view: ({el}) => {
+    const attrs = el.attributes
+    const text = attrs.getNamedItem('text')?.value
+    const variant = attrs.getNamedItem('variant')?.value
+
+    if (!text || !variant) {
+      return span([])
+    }
+
+    el.removeAttribute('text')
+    el.removeAttribute('variant')
+
+    return span(
+      text,
+      {className: `${styles.label} ${styles[variant]}`}
+    )
+  },
+}
+
+document.querySelectorAll('.label')
+  .forEach((el) => {
+    mount(el, Label)
+  })

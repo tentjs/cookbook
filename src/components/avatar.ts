a6df297ee8c4d1800e97a06b8b88737fc85a057f
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2024-05-10 16:24:40 +0200

    feat(avatar): add width/height/size attrs

--- src/components/avatar.ts
+++ src/components/avatar.ts
@@ -5,21 +5,39 @@ const { div } = tags;
 const Avatar: Component = {
   view: ({ attr }) => {
     const src = attr('src');
+    const width = attr('width');
+    const height = attr('height');
+    const size = attr('size');
 
-    if (!src) {
+    if (!src && !size) {
       return div([]);
     }
 
+    const w = size ? `${size}px` : width ?? '50px';
+    const h = size ? `${size}px` : height ?? '50px';
+
     return div('', {
       style: `
-        background-image: url(${src});
-        width: 50px;
-        height: 50px;
-        border-radius: 50px;
+        background-image: url(${getSrc(src, size)});
+        width: ${w};
+        height: ${h};
+        border-radius: ${w};
       `,
     });
   },
 };
+
+function getSrc(src?: string, size?: string) {
+  if (src) {
+    return src;
+  }
+
+  if (size) {
+    return `https://source.unsplash.com/${size}x${size}`;
+  }
+
+  return '';
+}
 
 document.querySelectorAll('.avatar').forEach((el) => {
   mount(el, Avatar);

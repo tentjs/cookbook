2c92605e68f0c97f153ef1e980de1f9d3d6de704
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2024-05-11 20:07:03 +0200

    refactor: use `attr()` for data passing recipe

--- src/components/attributes.ts
+++ src/components/attributes.ts
@@ -6,18 +6,14 @@ type State = {
   msg: string;
 };
 
-const AttributePassing: Component<State> = {
+const Attributes: Component<State> = {
   state: { msg: '' },
   view: ({ state }) => p(`This is your message: ${state.msg}`),
-  mounted: ({ state }) => {
-    const el = document.querySelector('.recipe');
-    if (!el) return;
+  mounted: ({ state, attr }) => {
+    const msg = attr('msg');
 
-    state.msg =
-      el.attributes.getNamedItem('msg')?.value ?? 'Attribute `msg` was not set';
-
-    el.removeAttribute('msg'); // optional
+    state.msg = msg ? msg : 'No message provided';
   },
 };
 
-mount(document.querySelector('.recipe'), AttributePassing);
+mount(document.querySelector('.recipe'), Attributes);
